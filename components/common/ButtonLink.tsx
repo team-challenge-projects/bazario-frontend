ac4a80af9c2ee140595629dfdcc4a3d8b3849c40
Volodymyr Basok <volodymyrquo@gmail.com>
2025-02-05 18:51:31 +0200

    Test Husky

---
+++ components/common/ButtonLink.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { FC } from "react";
+import Link from "next/link";
+import Button from "@/app/components/common/Button";
+import { ButtonLinkProps } from "@/app/lib/ButtonLink.types";
+
+const ButtonLink: FC<ButtonLinkProps> = ({ url, ...buttonProps }) => {
+  return (
+    <Link href={url}>
+      <Button {...buttonProps} />
+    </Link>
+  );
+};
+
+export default ButtonLink;
