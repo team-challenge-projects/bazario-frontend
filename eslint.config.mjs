7014d9c5a25948618cc73778824bc38f570139ef
Volodymyr Basok <volodymyrquo@gmail.com>
2025-02-05 19:30:28 +0200

    Add prettier and config it

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["prettier"], // Додаємо плагін Prettier
+    rules: {
+      "prettier/prettier": "error", // Включаємо правила Prettier
+    },
+  },
+  {
+    // Вимикаємо правила ESLint, які конфліктують з Prettier
+    extends: ["plugin:prettier/recommended"],
+  },
 ];
 
 export default eslintConfig;
